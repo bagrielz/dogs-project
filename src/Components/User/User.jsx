aca4c7316b6224bf754e5774ba5d1ad5856805e5
Gabriel Stênio <gabri.stnio50@hotmail.com>
2024-01-12 19:02:26 -0300

    Adicionado o scroll infinito.

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -4,13 +4,16 @@ import UserPhotoPost from "./UserPhotoPost/UserPhotoPost";
 import UserStats from "./UserStats";
 import Feed from "../Feed/Feed";
 import { Routes, Route } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
