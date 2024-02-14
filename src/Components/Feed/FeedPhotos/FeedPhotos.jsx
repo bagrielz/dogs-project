f8d107a0b64031c60a8bb9eb16e475f064aa4630
Gabriel Stênio <gabri.stnio50@hotmail.com>
2024-02-14 15:56:33 -0300

    Corrigido o problema no scroll infinito.

--- src/Components/Feed/FeedPhotos/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos/FeedPhotos.jsx
@@ -15,9 +15,7 @@ const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
       const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      console.log(json);
-
-      if (response && response.ok && json.lenght < total) setInfinite(false);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
   }, [request, user, page, setInfinite]);
